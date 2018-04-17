123287729b2b94d570746dbe13cb74bfab704a1b
wuying <wuy_123@126.com>
2018-04-17 11:42:28 +0800

    wuyangzl

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -2,26 +2,52 @@
 	class User extends CI_Controller{
 		public function __construct(){
 			parent::__construct();
+			$this->load->model('User_model');
 		}
-		
+
 		public function index(){
-			
+			header('Access-Control-Allow-Origin:* ');
+			header('Access-Control-Allow-Headers: X-Requested-With, Content-Type');
+			$country = $this->input->post('country');
+			$town = $this->input->post('town');
+			$province = $this->input->post('province');
+			$dept = $this->User_model->get_dept($country,$town,$province);
+			if($dept == null){
+				$rows = $this->User_model->save_dept($country,$town,$province);
+				if($rows > 0){
+					$dept = $this->User_model->get_dept($country,$town,$province);
+				}
+			}
+			echo $dept;
 		}
-		
+
+		//用户注册功能
 		public function reg(){
-			
+			header('Access-Control-Allow-Origin:* ');
+			header('Access-Control-Allow-Headers: X-Requested-With, Content-Type');
+			$dept_id = $this->input->post('dept_id');
+			$name = $this->input->post('name');
+			$pass = $this->input->post('pass');
+			$rows = $this->User_model->save_user($dept_id,$name,$pass);
+			if($rows > 0){
+				echo 'success';
+			}else{
+				echo 'fail';
+			}
 		}
-		
-		public function do_reg(){
-			
-		}
-		
+		//用户登录功能
 		public function login(){
-			
-		}
-		
-		public function do_login(){
-			
+			header('Access-Control-Allow-Origin:* ');
+			header('Access-Control-Allow-Headers: X-Requested-With, Content-Type');
+			$dept_id = $this->input->post('dept_id');
+			$name = $this->input->post('name');
+			$pass = $this->input->post('pass');
+			$rows = $this->User_model->get_user($dept_id,$name,$pass);
+			if($rows > 0){
+				echo 'success';
+			}else{
+				echo 'fail';
+			}
 		}
 	}
 
